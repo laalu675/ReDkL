8989621a2563235fb78cd6d09fad919e6b4c4607
amit747gt <5b191db8dd9fd688@gimbmail.com>
2025-07-12 13:31:31 +0530

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -20,42 +20,50 @@ echo "$username:$password" | sudo chpasswd
 sudo sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 echo "User created and configured with username '$username' and password '$password'"
 
-echo "Installing necessary packages and the full Ubuntu Desktop"
+echo "Installing Ubuntu Desktop and necessary packages"
 export DEBIAN_FRONTEND=noninteractive
 sudo apt update
 sudo apt install --assume-yes ubuntu-desktop wget
-
-echo "Setting up Chrome Remote Desktop"
-echo "Installing Chrome Remote Desktop"
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
-
-echo "Configuring Chrome Remote Desktop for Ubuntu's GNOME session"
-echo "exec /usr/sbin/lightdm-session 'gnome-session --session=ubuntu'" | sudo tee /etc/chrome-remote-desktop-session
 
 echo "Installing Google Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 
+echo "Installing Chrome Remote Desktop"
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+
+echo "Disabling the default display manager to prevent conflicts"
+sudo systemctl disable gdm3.service
+
+echo "Configuring Chrome Remote Desktop to use the GNOME session"
+sudo tee /etc/chrome-remote-desktop-session > /dev/null <<EOL
+export XDG_SESSION_TYPE=x11
+export GNOME_SHELL_SESSION_MODE=ubuntu
+exec /usr/bin/gnome-session
+EOL
+
 # Prompt user for CRP value
 read -p "Enter CRP value: " CRP
 
-echo "Finalizing"
+echo "Finalizing Setup"
 if [ "$Autostart" = true ]; then
     mkdir -p "/home/$username/.config/autostart"
     link="https://youtu.be/d9ui27vVePY?si=TfVDVQOd0VHjUt_b"
     colab_autostart="[Desktop Entry]\nType=Application\nName=Colab\nExec=sh -c 'sensible-browser $link'\nIcon=\nComment=Open a predefined notebook at session signin.\nX-GNOME-Autostart-enabled=true"
     echo -e "$colab_autostart" | sudo tee "/home/$username/.config/autostart/colab.desktop"
     sudo chmod +x "/home/$username/.config/autostart/colab.desktop"
-    sudo chown "$username:$username" "/home/$username/.config"
+    sudo chown "$username:$username" "/home/$username/.config" -R
 fi
 
 sudo adduser "$username" chrome-remote-desktop
 command="$CRP --pin=$Pin"
+
+echo "Starting Chrome Remote Desktop service for user $username"
 sudo su - "$username" -c "$command"
 sudo service chrome-remote-desktop start
 
-echo "Finished Successfully"
+echo "Finished Successfully. The remote desktop should be available shortly."
 while true; do sleep 10; done
